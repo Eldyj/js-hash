63ff37cc24ae022def3a64f0282f05571753ba7f
Eldyj <103959122+Eldyj@users.noreply.github.com>
2022-08-04 15:56:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
 class Hash {
 	constructor(...hashes){
-
 		let hash = this;
 		hash.indexes = [];
 		hash.values = [];
-
-		hash.get = index => {
-			return hash.values[hash.indexes.indexOf(index)];
-		}
-
-		hash.indexOf = value => {
-			return hash.indexes[hash.values.indexOf(value)];
-		}
-
-		if (hashes.length > 3) {
-			hash.pop = () => {
-				return new Hash(hash.indexes.pop(), hash.values.pop());
-			}
-
-			hash.shift = () => {
-				return new Hash(hash.indexes.shift(), hash.values.shift());
-			}
-		}
-
-		hash.set = (index, value) => {
-			hash.values[hash.indexes.indexOf(index)] = value;
-		}
-
+		hash.type = 'hash';
+		hash.length = () => hash.indexes.length;
+		hash.get = index => hash.values[hash.indexes.indexOf(index)];
+		hash.indexOf = value => hash.indexes[hash.values.indexOf(value)];
+		hash.pop = () => new Hash(hash.indexes.pop(), hash.values.pop());
+		hash.shift = () => new Hash(hash.indexes.shift(), hash.values.shift());
+		hash.set = (index, value) => hash.values[hash.indexes.indexOf(index)] = value;
 		hash.push = (index, value) => {
 			hash.indexes.push(index);
 			hash.values.push(value);
 		}
-
 		for (let i = 0; i < hashes.length; i++) {
-			let el = hashes[i];
-			i % 2 == 0 ? hash.indexes.push(el) : hash.values.push(el);
+			i % 2 == 0 ? hash.indexes.push(hashes[i]) : hash.values.push(hashes[i]);
 		}
-		hash.type = 'hash';
-		hash.length = hash.indexes.length;
 	}
 }
